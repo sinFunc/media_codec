3a782181dcac6ea225bc375175dc134c071c2007
苏旭彬 <19802021377@139.com>
2024-02-04 11:31:44 +0800

    first commit

---
+++ Icodec.cpp
@@ -0,0 +1,20 @@
+//
+// Created by sean on 2024/2/2.
+//
+#include <iostream>
+#include "Icodec.h"
+
+
+namespace mediaCodec{
+
+
+void Icodec::Test()
+{
+    std::cout<<__func__ <<std::endl;
+}
+
+
+
+
+
+}//namespace mediaCodec
---
+++ Icodec.h
@@ -0,0 +1,17 @@
+//
+// Created by sean on 2024/2/2.
+//
+
+#ifndef MEDIA_CODEC_ICODEC_H
+#define MEDIA_CODEC_ICODEC_H
+
+namespace mediaCodec{
+
+
+class Icodec {
+public:
+    void Test();
+};
+
+}//namespace mediaCodec
+#endif //MEDIA_CODEC_ICODEC_H
